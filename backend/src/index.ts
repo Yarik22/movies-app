7dc9e3229f00766d18fc6d0ec7112e5c91bf8fea
Yarik22 <popov.yarik.popov@gmail.com>
2025-08-31 14:32:51 +0300

    fixed CORS

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -5,9 +5,11 @@ import moviesRouter from "./routes/movies.js";
 
 const app = express();
 
+const CLIENT = process.env.CLIENT || "http://localhost:5173";
+
 app.use(
   cors({
-    origin: "http://localhost:5173",
+    origin: CLIENT,
     methods: ["GET", "POST", "PUT", "PATCH", "DELETE"],
     allowedHeaders: ["Content-Type", "x-username"],
   })
@@ -16,8 +18,7 @@ app.use(
 app.use(express.json());
 app.use("/movies", moviesRouter);
 
-const CLIENT = process.env.CLIENT || "http://localhost:4000";
 const PORT = process.env.PORT || "4000";
 app.listen(PORT, () => {
-  console.log(`Server running on ${CLIENT}`);
+  console.log(`Server running on ${PORT}`);
 });
